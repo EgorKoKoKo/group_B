42cf80c0aba0d2a97c43a957d3f07d46be1accc6
egor <egorkokoko@gmail.com>
2024-01-16 21:24:34 +0100

    101

---
+++ app.js
@@ -0,0 +1,54 @@
+// DO NOT CHANGE!
+//init app with express, util, body-parser, csv2json
+const express = require('express');
+const app = express();
+const sys = require('util');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+//register body-parser to handle json from res / req
+app.use( bodyParser.json() );
+
+//register public dir to serve static files (html, css, js)
+app.use( express.static( path.join(__dirname, "public") ) );
+
+// END DO NOT CHANGE!
+let rawdata = fs.readFileSync('data.json');
+//formating
+let data = JSON.parse(rawdata);
+
+
+/**************************************************************************
+****************************** Get request  ********************************
+**************************************************************************/
+app.get('/', (req ,res) => {
+	res.sendFile(path.resolve("public/map.html"));
+});
+
+app.get('/alphabetic', (req ,res) => {
+	res.sendFile(path.resolve("public/alph.html"));
+});
+
+app.get('/category', (req ,res) => {
+	res.sendFile(path.resolve("public/category.html"));
+});
+
+app.get('/account', (req ,res) => {
+	res.sendFile(path.resolve("public/account.html"));
+});
+
+/**************************************************************************
+*************************** handle HTTP METHODS ***************************
+**************************************************************************/
+app.get('/data', (req ,res) => {
+	res.send(data);
+});
+
+
+// DO NOT CHANGE!
+// bind server localhost to port 3000
+const port = 3000;
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ public/js/map.js
@@ -0,0 +1,92 @@
+let dresden = [51.050407, 13.737262]
+let markers = new Array();
+
+// map setup
+var map = L.map('map', {attributionControl: false} ).setView(dresden, 15);
+var myAttrControl = L.control.attribution().addTo(map);
+myAttrControl.setPrefix('<a href="https://leafletjs.com/">Leaflet</a>');
+
+// create layers
+const attribution = 'Groupe B: Prototype'
+
+var normal = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+subdomains: 'abcd',
+	maxZoom: 19,
+    attribution: attribution
+}).addTo(map);
+
+var dark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+subdomains: 'abcd',
+	maxZoom: 19,
+    attribution: attribution
+});
+
+var white = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    maxZoom: 19,
+    attribution: attribution
+});
+
+var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+   maxZoom: 19,
+   subdomains:['mt0','mt1','mt2','mt3'],
+   attribution: attribution
+});
+
+var baseMaps = {
+    "Normal": normal,
+    "Dark": dark,
+    "White": white,
+    "Satellit": googleSat 
+};
+
+L.control.layers(baseMaps).addTo(map);
+
+//get data from server
+$.ajax({
+    url: 'http://localhost:3000/data',
+    contentType: 'application/json',
+    success: function setIcons(res) {
+        res.forEach(function (dataitem) {
+        let marker = L.marker(dataitem.coordinate).addTo(map)
+        .bindPopup("<a class='buttom_about_sport' title='read more'> "+dataitem.name + "</a><hr> Groups: <br>"+ printAllgroups(dataitem.groups), {closeOnClick: false, autoClose: false});
+        var newIcon = new L.Icon({
+            iconUrl: dataitem.icon,
+            iconSize: [30, 50],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+        marker.setIcon(newIcon);
+        markers.push(marker);
+        });
+    }   
+});
+
+//asking location
+map.locate({setView: true, maxZoom: 16});
+map.on('locationfound', e => {
+    //var radius = e.accuracy;
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are here!!!").setIcon(
+            new L.Icon({
+            iconUrl: "img/you.svg",
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        })).openPopup();
+    //L.circle(e.latlng, radius).addTo(map).setStyle({color: 'black'});;
+});
+
+
+// other functions
+function printAllgroups(groups) {
+    let end_groups_text = "";
+    groups.forEach((group) => {
+        let buttom_enroll = "<a class='buttom_e'> enroll </a>";
+        let buttom_read_more = "<a class='buttom_rm'> read more </a>";
+        end_groups_text = end_groups_text+ "<br><strong>" + group.group_name + "</strong><br>" + group.day+ " " + group.time  + "<br>  cost: " + group.cost + " by " + group.tranier_name + 
+        "<br>" + buttom_read_more + buttom_enroll + "<br>"; 
+        })
+    return end_groups_text;
+}
